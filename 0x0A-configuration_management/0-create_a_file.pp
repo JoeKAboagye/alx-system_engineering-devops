725070dcce1b1236e09134634750da4c274973bb
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2022-01-03 06:15:25 -0800

    Creating a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 # Creating a file in /tmp that satisfies the requirements below
 
-$doc_root = "/tmp/school"
+$doc_root = '/tmp/school'
 
 file {$doc_root:
-  ensure  => "file",
-  mode    => 0744,
-  owner   => "www-data",
-  group   => "www-data",
-  content => "I love Puppet"
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
